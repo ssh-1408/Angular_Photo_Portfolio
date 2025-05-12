7f30ba728de674c1887840d06e53392cd1f0f129
Svetlana Shmeleva <svetlana.shmeleva.1@gmail.com>
2025-05-12 11:38:03 +0200

    update project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,11 @@ import { RouterLink } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { SectionsComponent } from './sections/sections.component';
 import { AchievementsComponent } from './achievements/achievements.component';
-import { TestimonialsComponent } from '../testimonials/testimonials.component';
+
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, HeroComponent, SectionsComponent, AchievementsComponent, TestimonialsComponent],
+  imports: [RouterLink, HeroComponent, SectionsComponent, AchievementsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,5 +8,12 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  @ViewChild('navbarCollapse', { static: false }) navbarCollapse!: ElementRef;
 
+  closeNavbar() {
+    const element = this.navbarCollapse.nativeElement;
+    if (element.classList.contains('show')) {
+      element.classList.remove('show'); // Manually collapse Bootstrap navbar
+    }
+  }
 }
